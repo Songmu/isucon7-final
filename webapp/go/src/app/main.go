e58cbfd5722ac5d26a6aadb3e359cf5760f83009
Songmu <y.songmu@gmail.com>
2017-11-25 12:05:10 +0900

    local db見に行くように

--- webapp/go/src/app/main.go
+++ webapp/go/src/app/main.go
@@ -31,9 +31,7 @@ var localServers = []string{
 
 func initDB() {
 	db_host := os.Getenv("ISU_DB_HOST")
-	if db_host == "" {
-		db_host = "127.0.0.1"
-	}
+	db_host = "127.0.0.1"
 	db_port := os.Getenv("ISU_DB_PORT")
 	if db_port == "" {
 		db_port = "3306"
